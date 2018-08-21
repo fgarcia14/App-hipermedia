e176f39a238a08bbcbe3f0bf6337b4e5911f8a9d
Fidel GB <fgarcia14@ucol.mx>
2018-08-21 08:24:23 -0500

    First commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hola mundo!');
-});
+app.get('/', (req, res) => {
+    res.send("Hola mundo!!");
+})
 
-const PORT=process.env.PORT || 4000;
+let PORT = process.env.PORT || 4000;
 
-app.listen(4000, function () {
-  console.log('Escuchando en el puerto 4000');
+app.listen(PORT, function() {
+  console.log('Servidor escuchando en puerto 4000');
 });
