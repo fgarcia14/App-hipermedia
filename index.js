48a7619eae4f23816c586fc02cd1f04fa91cdfa2
Fidel <fgarcia14@ucol.mx>
2018-08-23 07:33:23 -0500

    Prueba

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send({bye: 'budy'});
+    res.send({bye: 'budy', prueba: 'HelloWorld!'});
 })
 
 let PORT = process.env.PORT || 4000;
